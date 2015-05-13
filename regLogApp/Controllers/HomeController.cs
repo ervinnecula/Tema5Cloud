3d816b23b6feddc6c75ca7db43d1f42558cbdb64
Ervin Necula <ervinnecula@yahoo.com>
2015-05-13 21:40:02 +0300

    Added All Project

---
+++ regLogApp/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AzureDb;
+
+namespace regLogApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            ViewBag.Message = "Your schedule page.";
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            ViewBag.Message = "Logout page";
+
+            return View();
+        }
+        // GET: User
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new ervinEntities())
+                {
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                    ModelState.Clear();
+                    user = null;
+                    ViewBag.Message = "Registration Successfully Done";
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.LoginUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(user.UserName, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login data is incorect!");
+                }
+            }
+            return View(user);
+        }
+     
+    }
+}
---
+++ regLogApp/Models/LoginUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using AzureDb;
+
+namespace regLogApp.Models
+{
+    public class LoginUser
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public bool IsValid(string username, string password)
+        {
+            using (var context = new ervinEntities())
+            {
+                if (Enumerable.Any(context.Users, user => user.UserName == username && user.Password == password))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
