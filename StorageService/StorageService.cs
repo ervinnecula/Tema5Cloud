d7635572ceba8f1eb91e8ff687c1144e97750f8b
Alin <alincosmin123@gmail.com>
2015-05-14 00:18:55 +0300

    StorageService created.

---
+++ StorageService/StorageService.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+
+namespace StorageService
+{
+    public class StorageService
+    {
+        private CloudStorageAccount _storage;
+
+        public StorageService()
+        {
+            _storage = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["Storage"].ToString());
+        }
+    }
+}
